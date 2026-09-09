9603ac0ee226ff158d23e5ab82eddcf2ccd971e1
andrei <villarantechristianandrie@gmail.com>
2026-09-09 22:27:12 +0800

    lava

--- app/config/routes.php
+++ app/config/routes.php
@@ -52,6 +52,9 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
     require_once APP_DIR . 'config/middleware.php';
     get_config($config);
 })();
+
+$router->get('/', 'Welcome::index');
+
 /*
 $router->get('/', 'Welcome::index');
 
