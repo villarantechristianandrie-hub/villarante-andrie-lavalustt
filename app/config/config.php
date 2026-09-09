92359596d8d58fa1ec697d3473971a310fb73934
andrei <villarantechristianandrie@gmail.com>
2026-09-09 22:34:43 +0800

    Fix deployed base URL generation

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,11 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost:3000/';
+$config['base_url'] = getenv('APP_URL') ?: (
+	isset($_SERVER['HTTP_HOST'])
+		? ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/')
+		: 'http://localhost:3000/'
+);
 
 /*
 |--------------------------------------------------------------------------
